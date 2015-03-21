e181f27ce22007d99de6b8eefdc57dd11aea065f
Ariel Silva Jr <ceosilvajr@gmail.com>
2015-03-22 02:23:25 +0800

    This class is used to check internet connection

--- src/main/java/ph/coreproc/android/bursthttp/utils/InternetChecker.java
+++ src/main/java/ph/coreproc/android/bursthttp/utils/InternetChecker.java
@@ -1,7 +1,30 @@
 package ph.coreproc.android.bursthttp.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by ceosilvajr on 3/22/15.
  */
 public class InternetChecker {
+
+    private Context context;
+
+    public InternetChecker(Context context) {
+        this.context = context;
+    }
+
+    public boolean isConnected() {
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            return activeNetwork != null
+                    && activeNetwork.isConnectedOrConnecting();
+        } catch (NullPointerException e) {
+            // nothing
+        }
+        return true;
+    }
 }
